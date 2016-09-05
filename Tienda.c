f8c4463c986e47e40c011dc75b4319640cd8ccc5
DakeDekaane <servantarcherubw@gmail.com>
2016-09-05 01:53:40 -0500

    Se añadió el sistema de logueo en el programa.

---
+++ Tienda.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum boolean{false,true};
+
+//int num_usuarios=2;
+//int tamano_contrasena=5;
+//int tamano_usuario=10;
+
+char usuarios[2][10]={"donaldo","2"};
+char contrasenas[2][5]={"1994","1991"};
+
+int intentos=3;	
+int check=0;
+
+enum boolean valida_usuario(){
+
+	char usuarioIngresado[16]={'\0'}, contrasenaIngresada[16]={'\0'};
+	do {
+		printf("Usuario: ");
+		scanf(" %[^\n]",usuarioIngresado);
+		printf("Contraseña: ");
+		scanf(" %[^\n]",contrasenaIngresada);
+	
+		for (int i=0;i<2;i++) {
+			if( strcmp(usuarioIngresado,usuarios[i]) == 0 && strcmp(contrasenaIngresada,contrasenas[i]) ==0 ) {
+				printf("Acceso autorizado. Bienvenido, %s.\n",usuarioIngresado);
+				check = 1;
+				break;
+			}
+			else {
+				continue;
+			}
+		}
+		
+		if ( check == 0 ) {
+			printf("Acceso denegado. Verifique sus datos\n");
+			intentos--;
+		}
+		else {
+			continue;
+		}
+
+	} while(intentos > 0 && check == 0 );
+
+	if (intentos == 0) printf("Ha realizado 3 intentos fallidos, se le reportará al servidor\n\n");
+	return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+	printf("\t\tPrebeTienda\n");
+
+	valida_usuario();
+	return 0;
+}
