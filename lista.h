8751451b47e8180c065b2750a872da235513b2a3
DakeDekaane <servantarcherubw@gmail.com>
2016-09-08 23:51:35 -0500

    Se terminó la biblioteca de lista con las funciones necesarias.

--- lista.h
+++ lista.h
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum boolean{false,true};
 
 typedef struct Nodo {
-	char *nombreProducto;
+	char nombreProducto[20];
 	int cantidad;
 	float precio;
 	struct Nodo* siguiente;
 }Nodo;
 
-Nodo* inicio=NULL;	//Inicio de cola
-Nodo* final=NULL;	//Final de cola
+Nodo* inicio=NULL;
+Nodo* final=NULL;
 
-Nodo* crearNodo(char *nombreProducto, int cantidad, float precio) {
+Nodo* crearNodo(char nombreProducto[], int cantidad, float precio) {
 	Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
-	nuevo->sucesor = NULL;
-	nuevo->antecesor = NULL;
-	nuevo->nombreProducto = nombreProducto;
+	nuevo->siguiente = NULL;
+	strcpy(nuevo->nombreProducto,nombreProducto);
 	nuevo->cantidad = cantidad;
 	nuevo->precio = precio;
 	return nuevo;
@@ -42,6 +42,98 @@ void InsertaNodo(Nodo* nodo) {
 }
 
 //Remueve elemento por nombre.
-Nodo* RemueveNodo(char *nombre) {
-	printf("codigo\n");
+Nodo* RemueveNodo(char nombreProducto[]) {
+	//Si la lista está vacía.
+	if(ListaVacia()) {
+		return (Nodo*)NULL;
+	}
+	Nodo* aux = inicio;
+	//Si la lista tiene un sólo elemento.
+	if (inicio == final && strcmp(aux->nombreProducto,nombreProducto) == 0 ) {
+		inicio = NULL;
+		final = NULL;
+		return aux;
+	}
+	else{
+		Nodo* scout = inicio;
+		while( strcmp(scout->nombreProducto,nombreProducto) != 0 ) {
+			scout = scout->siguiente;
+			if(scout == NULL) return NULL;
+		}
+		
+		while(aux->siguiente != scout) {
+			aux = aux->siguiente;
+		}
+		aux->siguiente = scout->siguiente;
+		return scout;
+	}
+}
+
+void BuscaNodo(char nombreProducto[]) {
+	if(ListaVacia()) {
+		printf("Lista Vacía.\n");
+	}
+	else {
+		int opcion;
+		Nodo* scout = inicio;
+		while( strcmp(scout->nombreProducto,nombreProducto) != 0 ) {
+			scout = scout->siguiente;
+			if (scout == NULL) {
+				printf("Producto \"%s\" no encontrado.\n",nombreProducto);
+				return;
+			}
+		}
+	printf("Nombre: %s\t Cantidad: %d\tPrecio: %.2f\n",scout->nombreProducto,scout->cantidad,scout->precio);	
+	}
+}
+
+void ImprimeNodos() {
+	//Si la lista está vacía.
+	if(ListaVacia()) {
+		printf("Lista Vacía.\n");
+	}
+	else {
+		Nodo* aux = inicio;
+		printf(" Nombre \t Cantidad \t Precio\n");
+		while(aux != NULL) {
+			if(strlen(aux->nombreProducto) <= 6) 
+				printf(" %s\t\t    %d\t\t %.2f\n",aux->nombreProducto,aux->cantidad,aux->precio);
+			else
+				printf(" %s\t    %d\t\t %.2f\n",aux->nombreProducto,aux->cantidad,aux->precio);
+			aux = aux->siguiente;
+		}	
+	}
+}
+
+void ModificaNodo(char nombreProducto[]) {
+	//Si la lista está vacía.
+	if(ListaVacia()) {
+		printf("Lista Vacía.\n");
+	}
+	else {
+		int opcion;
+		Nodo* scout = inicio;
+		while( strcmp(scout->nombreProducto,nombreProducto) != 0 ) {
+			scout = scout->siguiente;
+			if (scout == NULL) {
+				printf("Producto \"%s\" no encontrado.\n",nombreProducto);
+				return;
+			}
+		}
+		printf("¿Qué desea modificar?\n1. Cantidad\n2. Precio\nIngrese una opción: ");
+		scanf("%d",&opcion);
+		switch(opcion) {
+			case 1:
+				printf("Ingrese nueva cantidad: ");
+				scanf("%d",&(scout->cantidad));
+				break;
+			case 2:
+				printf("Ingrese nuevo precio: ");
+				scanf("%f",&(scout->precio));
+				break;
+			default:
+				printf("Opción inválida.");
+				break;
+		}
+	}
 }
