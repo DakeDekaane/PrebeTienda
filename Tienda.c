425bdf799d6a939f50823508c14720450dd68e30
DakeDekaane <servantarcherubw@gmail.com>
2016-09-08 23:52:15 -0500

    Se incluyó la biblioteca de lista.h

--- Tienda.c
+++ Tienda.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lista.h"
 
-enum boolean{false,true};
-
+//enum boolean{false,true};
 
 //int num_usuarios=2;
 //int tamano_contrasena=5;
@@ -12,8 +12,50 @@ enum boolean{false,true};
 char usuarios[2][10]={"donaldo","2"};
 char contrasenas[2][5]={"1994","1991"};
 
+
+
 int intentos=3;	
 int check=0;
+
+enum boolean valida_usuario();
+
+
+int main(int argc, char const *argv[])
+{
+	printf("\t\tPrebeTienda\n");
+
+	valida_usuario();
+
+	FILE *archivo = fopen("Productos.txt","a+");
+	if (!archivo) {
+		printf("Error al cargar base de datos. Verifique el archivo.\n");
+		return 1;
+	}
+
+	char linea[30];
+	fscanf(archivo,"%s",linea);
+	printf("%s\n",linea);
+	fclose(archivo);
+
+	InsertaNodo(crearNodo("Papas",10,8.00));	
+	InsertaNodo(crearNodo("Refresco",15,9.50));
+	InsertaNodo(crearNodo("Leche",2,100.00));
+	InsertaNodo(crearNodo("Chocolate",30,0.50));
+	InsertaNodo(crearNodo("Cafe",25,2.50));
+	ImprimeNodos();
+	printf("\n");
+	RemueveNodo("Refresco");
+	ImprimeNodos();
+	printf("\n");
+	ModificaNodo("Dulces");
+	ModificaNodo("Papas");
+	ImprimeNodos();
+	printf("\n");
+	BuscaNodo("Leche");
+	BuscaNodo("Agua");
+	
+	return 0;
+}
 
 enum boolean valida_usuario(){
 
@@ -49,22 +91,3 @@ enum boolean valida_usuario(){
 	return 1;
 }
 
-int main(int argc, char const *argv[])
-{
-	printf("\t\tPrebeTienda\n");
-
-	valida_usuario();
-
-	FILE *archivo = fopen("Productos.txt","a+");
-	if (!archivo) {
-		printf("Error al cargar base de datos. Verifique el archivo.\n");
-		return 1;
-	}
-
-	char linea[30];
-	fscanf(archivo,"%s",linea);
-	printf("%s\n",linea);
-	fclose(archivo);
-	
-	return 0;
-}
